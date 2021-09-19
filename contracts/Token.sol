72287bd9e0705cfff3453dbab183a3e5f5ecda44
modernAlcibiades <mehul.kumar@columbia.edu>
2021-09-19 19:53:20 +0900

    Updated hardhat config

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,8 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./Priest.sol";
 
 contract Token is ERC20("Dragon Priest Token", "DPT") {
     Priest priest;
 
-    constructor(add) {}
+    constructor() {}
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
@@ -15,7 +16,49 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
- */
+*/
+
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.6",
+
+  networks: {
+    hardhat: {
+      forking: {
+        chainId: 250,
+        url: "https://rpc.ftm.tools/",
+        blockNumber: 17291067,
+        accounts: [process.env.PRIVATE_KEY]
+      }
+    },
+    fantom: {
+      chainId: 250,
+      url: "https://rpc.ftm.tools/",
+      accounts: [process.env.PRIVATE_KEY],
+      maxFeePerGas: 190000000000
+    },
+  },
+  "evmVersion": "berlin",
+  "libraries": {},
+  "metadata": {
+    "bytecodeHash": "ipfs",
+    "useLiteralContent": true
+  },
+  "optimizer": {
+    "enabled": true,
+    "runs": 200
+  },
+  "remappings": [],
+  "outputSelection": {
+    "*": {
+      "*": [
+        "evm.bytecode",
+        "evm.deployedBytecode",
+        "abi"
+      ]
+    }
+  },
+  mocha: {
+    timeout: 100000
+  }
 };
+
